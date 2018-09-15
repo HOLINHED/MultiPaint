2a3466bf8f072dcfabcb9edadf0f6fe189336464
Holinhed <synergycocyt@gmail.com>
2018-09-15 15:25:59 -0400

    file upload

---
+++ public/client.js
@@ -0,0 +1,55 @@
+const socket = io('http://localhost:3000');
+let clientColor;
+let px,py,size;
+let con = false;
+
+function setup(){
+    createCanvas(800,800);
+    background(0);
+    noStroke();
+
+    size = 25;
+
+    clientColor = {
+        r:random(255),
+        g:random(255),
+        b:random(255)
+    };
+
+    socket.on("mouse", (data)=>{
+        fill(data.c.r,data.c.g,data.c.b);
+        ellipse(data.x,data.y,data.s,data.s);
+    });
+    socket.on("id", (idData)=>document.getElementById("client_id").innerHTML = "id: " + idData.id);
+}
+
+function mouseDragged(){
+    fill(255);
+    ellipse(mouseX,mouseY,size,size);
+
+
+    let data = {
+        x: mouseX,
+        y: mouseY,
+        s:size,
+        c: clientColor
+    };
+
+    socket.emit("mouse",data);
+}
+
+function keyPressed(){
+    document.getElementById("client_size").innerHTML = "size: " + size;
+    switch(keyCode){
+        case 38: 
+            if (size < 75){
+                size++;
+            };
+        break;
+        case 40: 
+            if (size > 10){
+                size--;
+            };
+        break;
+    }
+}
---
+++ server.js
@@ -0,0 +1,23 @@
+const PORT = 80;
+let express = require('express');
+let socket = require('socket.io');
+
+let app = express();
+let server = app.listen(PORT);
+let io = socket(server);
+
+app.use(express.static('public'));
+
+io.sockets.on('connection', (socket)=>{
+    let idData = {
+        id: socket.id,
+    };
+
+    io.sockets.emit("id", idData);
+
+    socket.on('mouse', (data)=>{
+        socket.broadcast.emit('mouse', data);
+    });
+});
+
+console.log('Server started on port ' + PORT);
