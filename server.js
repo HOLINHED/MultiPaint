38d3765196a0c9ba788b084ba5d679a2ebde8fdc
Dawid Klatkiewicz <klatkiewiczdawid@holinhed.com>
2019-01-04 18:37:26 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const PORT = process.env.PORT || 3000;
-let express = require('express');
-let socket = require('socket.io');
+const express = require('express');
+const socket = require('socket.io');
 
-let app = express();
-let server = app.listen(PORT);
-let io = socket(server);
+const app = express();
+
+const server = app.listen(PORT, () => console.log('running on port ' + PORT));
+const io = socket(server);
 
 app.use(express.static('public'));
 
-io.sockets.on('connection', (socket)=>{
-    socket.on('mouse', (data)=>{
+io.sockets.on('connection', socket => {
+    socket.on('mouse', data => {
         socket.broadcast.emit('mouse', data);
     });
 });
 
-console.log('Server started on port ' + PORT);
